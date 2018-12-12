61a4187b5350840d51b299543e463d5fb2980e47
paul <omd.paul@gmail.com>
2018-12-12 19:55:38 +0200

    sprint 1

--- SiteSnap/ViewController.swift
+++ SiteSnap/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var logo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UIView.animate(withDuration: 1, animations: {
+             self.logo.transform = CGAffineTransform(translationX: 0, y: -180)
+        }, completion: nil)
     }
 
 
