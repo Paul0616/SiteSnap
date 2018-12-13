4fb38439394ab8b6ce91d25c1f6619e5d0dc008e
Paul Oprea <omd.paul@gmail.com>
2018-12-13 15:11:54 +0200

    sprint 1 done

--- SiteSnap/AppDelegate.swift
+++ SiteSnap/AppDelegate.swift
@@ -13,10 +13,52 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    //MARK: - Set Orientation
+    /// set orientations you want to be allowed in this property by default
+    var orientationLock = UIInterfaceOrientationMask.all
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return self.orientationLock
+    }
+    struct AppUtility {
+        
+        static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
+            
+            if let delegate = UIApplication.shared.delegate as? AppDelegate {
+                delegate.orientationLock = orientation
+            }
+        }
+        
+        /// OPTIONAL Added method to adjust lock and rotate to the desired orientation
+        static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation:UIInterfaceOrientation) {
+            
+            self.lockOrientation(orientation)
+            
+            UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
+        }
+        
+    }
+    
+    //MARK: - Launcher Screen
+    private func splashScreen(){
+        let launchScreenVC = UIStoryboard.init(name: "LaunchScreen", bundle: nil)
+        let rootVC = launchScreenVC.instantiateViewController(withIdentifier: "splashScreen")
+        self.window?.rootViewController = rootVC
+        self.window?.makeKeyAndVisible()
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(dismissSplashController), userInfo: nil, repeats: false)
+    }
+    @objc func dismissSplashController(){
+        let mainVC = UIStoryboard.init(name: "Main", bundle: nil)
+        let rootVC = mainVC.instantiateViewController(withIdentifier: "initController")
+        self.window?.rootViewController = rootVC
+        self.window?.makeKeyAndVisible()
+    }
+    //MARK: -
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.splashScreen()
         return true
     }
 
--- SiteSnap/ViewController.swift
+++ SiteSnap/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var tapHereButton: UIButton!
@@ -20,16 +20,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        userNameTextField.delegate = self
+        passwordTextField.delegate = self
         loginButton.layer.cornerRadius = 6
         
         self.logo.alpha = 0
         
-        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 10, options: [.curveEaseOut], animations: {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 5, options: [.curveEaseOut], animations: {
             self.logo.transform = CGAffineTransform(translationX: 0, y: -180)
             self.logo.alpha = 1
         }, completion: nil)
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        AppDelegate.AppUtility.lockOrientation(.portrait)
+        // Or to rotate and lock
+        // AppUtility.lockOrientation(.portrait, andRotateTo: .portrait)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Don't forget to reset when view is being removed
+        AppDelegate.AppUtility.lockOrientation(.all)
+    }
+    
+    //MARK: - UItextfieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //hide keyboard
+        if textField == userNameTextField{
+            passwordTextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+            //attemptLogin()
+        }
+        return true
+    }
 }
 
