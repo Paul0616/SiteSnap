6bb4ee81bf90cb54ef4a9350975c4482368abf0b
Paul Oprea <omd.paul@gmail.com>
2018-12-13 11:49:32 +0200

    sprint 1

--- SiteSnap/ViewController.swift
+++ SiteSnap/ViewController.swift
@@ -11,13 +11,22 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var userNameTextField: UITextField!
-    
+    @IBOutlet weak var tapHereButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        UIView.animate(withDuration: 1, animations: {
-             self.logo.transform = CGAffineTransform(translationX: 0, y: -180)
+        loginButton.layer.cornerRadius = 6
+        
+        self.logo.alpha = 0
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 10, options: [.curveEaseOut], animations: {
+            self.logo.transform = CGAffineTransform(translationX: 0, y: -180)
+            self.logo.alpha = 1
         }, completion: nil)
     }
 
