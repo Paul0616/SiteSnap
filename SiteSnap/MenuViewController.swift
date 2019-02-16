e1be9cca728ee8f11126950ba6ab633a661d31ab
paul <omd.paul@gmail.com>
2019-02-16 08:46:00 +0200

    bugs

--- SiteSnap/MenuViewController.swift
+++ SiteSnap/MenuViewController.swift
@@ -12,6 +12,7 @@ import AWSCognitoIdentityProvider
 class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var versionLabel: UILabel!
     
     @IBOutlet weak var tableView: UITableView!
 
@@ -26,7 +27,9 @@ class MenuViewController: UIViewController, UITableViewDelegate, UITableViewData
         // Do any additional setup after loading the view.
         tableView.delegate = self
         failedPhotosNumber = PhotoHandler.getFailedUploadPhotosNumber()
-        
+        versionLabel.text = " Version "
+        let appVersion = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as? String
+        versionLabel.text = versionLabel.text?.appending(appVersion!)
         self.pool = AWSCognitoIdentityUserPool(forKey: AWSCognitoUserPoolsSignInProviderKey)
         if (self.user == nil) {
             self.user = self.pool?.currentUser()
