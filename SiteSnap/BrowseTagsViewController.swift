170fef59632fcc30e41a38cea3ee9ea9d176fb94
Paul Oprea <omd.paul@gmail.com>
2021-03-14 14:19:08 +0200

    a

--- SiteSnap/BrowseTagsViewController.swift
+++ SiteSnap/BrowseTagsViewController.swift
@@ -11,27 +11,40 @@ import UIKit
 class BrowseTagsViewController: UIViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
-    var tagsApplied = 0
+    var tagsApplied = 1
     
     @IBOutlet weak var allTagsTableView: UITableView!
-    @IBOutlet weak var tagsAppliedTableView: UITableView!
+    @IBOutlet weak var tagsAppliedStackView: UIStackView!
+    @IBOutlet weak var applyTagLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTablesView()
+    
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidLayoutSubviews() {
+        if self.view.safeAreaLayoutGuide.layoutFrame.size.width > self.view.safeAreaLayoutGuide.layoutFrame.size.height {
+            print("landscape")
+            tagsAppliedStackView.isHidden = false
+            applyTagLabel.isHidden = true
+        } else {
+            print("portrait")
+            applyTagLabel.isHidden = false
+            if tagsApplied == 0 {
+                tagsAppliedStackView.isHidden = true
+            } else {
+                tagsAppliedStackView.isHidden = false
+            }
+        }
+    }
+
 
     @IBAction func onBack(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
-    func setupTablesView() {
-        if tagsApplied == 0 {
-            tagsAppliedTableView.isHidden = true
-        }
-    }
+  
     /*
     // MARK: - Navigation
 
